bc3d44c38ecca2b69de81104d0531ff6c22b7b16
freeriderzer0 <103386251+freeriderzer0@users.noreply.github.com>
2023-03-17 02:43:10 +0300

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -4,16 +4,14 @@ import networkx as nx
 
 
 def maze_to_graph(is_wall, allowed_steps):
-    # map array indices to node indices and vice versa
     node_idx = list(range(np.sum(~is_wall)))
     node_pos = list(zip(*np.where(~is_wall)))
     pos2idx = dict(zip(node_pos, node_idx))
 
-    # create graph
     g = nx.DiGraph()
     for (i, j) in node_pos:
-        for (delta_i, delta_j) in allowed_steps:  # try to step in all allowed directions
-            if (i + delta_i, j + delta_j) in pos2idx:  # i.e. target node also exists
+        for (delta_i, delta_j) in allowed_steps:  
+            if (i + delta_i, j + delta_j) in pos2idx:  
                 g.add_edge(pos2idx[(i, j)], pos2idx[(i + delta_i, j + delta_j)],
                            weight=int((round((math.sqrt(abs(delta_i) + abs(delta_j))), 1)) * 10))
 
